dff5210df0356871d7a832f65ade05cbabbce404
Marvin <kern.marvin@outlook.de>
2025-01-15 00:02:17 +0100

    fix(service): Updated tests

--- src/test/java/de/thi/cnd/review/ReviewRestControllerV1Tests.java
+++ src/test/java/de/thi/cnd/review/ReviewRestControllerV1Tests.java
@@ -5,17 +5,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.thi.cnd.review.adapter.ingoing.rest.dto.CreateReviewRequestDTO;
 import de.thi.cnd.review.adapter.ingoing.rest.dto.UpdateReviewRequestDTO;
 import de.thi.cnd.review.adapter.outgoing.jpa.JpaReviewRepository;
+import de.thi.cnd.review.domain.UserService;
+import de.thi.cnd.review.domain.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +30,9 @@ class ReviewRestControllerV1Tests {
 
 	private final MockMvc mvc;
 	private final JpaReviewRepository reviewRepository;
+
+	@MockBean
+	private UserService userService;
 
 	@Autowired
 	public ReviewRestControllerV1Tests(MockMvc mvc, JpaReviewRepository reviewRepository) {
@@ -42,9 +49,12 @@ class ReviewRestControllerV1Tests {
 	void testCreateReview() throws Exception {
 		CreateReviewRequestDTO review = new CreateReviewRequestDTO();
 		review.setRecipeId("1L");
-		review.setAuthor("Max Mustermann");
+		review.setAuthor("mustermann");
 		review.setRating(5.0f);
 		review.setComment("Sehr lecker!");
+
+		when(userService.getUser("mustermann")).thenReturn(new User("mustermann", "Max", "Mustermann"));
+
 		mvc.perform(post("/api/v1/reviews")
 						.content(asJsonString(review))
 						.contentType(MediaType.APPLICATION_JSON)
@@ -52,7 +62,7 @@ class ReviewRestControllerV1Tests {
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.recipeId").value("1L"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.author").value("Max Mustermann"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.author").value("mustermann"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.rating").value(5.0f))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.comment").value("Sehr lecker!"));
 	}
